6757086a07aba9697b13f6f1b56dd22c1e5125e6
Eugenio <hedgehog0996@gmail.com>
2018-04-26 03:01:37 +0300

    add converter and connector

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const requestParse = require('./lib/request-parse');
 const transformers = require('./lib/transformes');
+const connectors = require('./lib/connector');
+const converter = require('./lib/converter');
 
 const server = http.createServer(listener);
 server.listen(3000);
@@ -15,20 +17,37 @@ function listener(req, res) {
   if (!properties.path) 
     return res.end(`Unknown system ${properties.system}`);
 
-  console.log(properties);
-
-  /* transform: xml or json */
   const Transformer = transformers[properties.format || 'xml'];
   if (!Transformer)
     return res.end('Unknown this format');
-  const transform = new Transformer();
-
   res.writeHead(200, {
     'Content-Type': Transformer.contentType
   });
+  const transform = new Transformer();
 
-  res.end('ok');
+  if (properties.method == 'GET' || properties.method == 'DELETE') {    
+    let connection = new connectors[properties.type]['withoutBody'](properties);
+    //let connect = connection.create();
 
+    if (properties.type == 'web')
+      connection.create().pipe(res);
+    else {
+      let connect = new connectors[properties.type]['withoutBody'](properties);
+      connect.pipe(transform).pipe(res);
+
+      transform.on('end', () => {
+        connection.close();
+        res.end();
+      });
+    }
+  } else {
+    let convert = new converter[properties.convertType]();
+    let connection = new connectors[properties.type]['withBody'](properties);
+    req.pipe(convert).pipe(connection).pipe(transform).pipe(res);
+    transform.on('end', () => {
+      res.end();
+    });
+  }
 }
 
 module.exports = server;
