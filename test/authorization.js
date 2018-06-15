d1be278116fc215de464520d3c2d5074767a7134
Eugenio <hedgehog0996@gmail.com>
2018-06-15 20:31:40 +0300

    changing authorization module

--- test/authorization.js
+++ test/authorization.js
@@ -1,26 +1,22 @@
-const tokens = require('./support/test.systems')['tokens'];
 const authorization = require('../lib/authorization');
 const expect = require('chai').expect;
 
 describe('Authorization', function() {
-  it('other length token', function(done) {
-    const access = authorization('eaaaaaaa', tokens);
-    expect(access).to.be.an.instanceof(Error);
-    done();
-  });
-
   it('token not true', function(done) {
-    const access = authorization('eaaaaaaa105c4f29a0f5117d2960926f', tokens);
-    expect(access).to.be.an.instanceof(Error);
-    done();
+    try {
+      authorization('ee135929105c4f29a0f5117d2960926f');
+    } catch (ex) {
+      expect(ex.message).to.equal('Access denied');
+      done();
+    }    
   });
 
   it('correct token', function(done) {
-    const access = authorization('ea135929105c4f29a0f5117d2960926f', tokens);
+    const access = authorization('ea135929105c4f29a0f5117d2960926f');
     expect(access['create']).to.equal(true);
     expect(access['read']).to.equal(true);
-    expect(access['update']).to.equal(false);
-    expect(access['delete']).to.equal(false);
+    expect(access['update']).to.equal(true);
+    expect(access['delete']).to.equal(true);
     done();
   });
 });
